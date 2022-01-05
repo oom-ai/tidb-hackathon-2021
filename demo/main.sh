f4a39c2ad0bff523cc031b55785466b54191a86c
Yik San Chan <evan.chanyiksan@gmail.com>
2022-01-05 15:47:50 +0800

    use external tidb and tikv

--- demo/main.sh
+++ demo/main.sh
@@ -44,7 +44,8 @@ info "generate oomstore schema..."
 # ffgen schema --recipe $RECIPE > oomstore.yaml
 
 info "initialize oomstore..."
-oomplay init tidb
+oomplay init tidbext
+oomplay init tikvext
 
 # give it 5 times to try
 for _i in {1..5}; do
